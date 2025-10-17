a691c437b28f250e8ec2baee79c9a5029b2b24af
Taras <t.kornev01@gmail.com>
2025-10-18 00:34:21 +0200

    Make View Gallery button black

---
+++ task1.py
@@ -0,0 +1,78 @@
+# Сума двох чисел: Написати програму, яка зчитує два цілих числа та виводить їх суму.
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Sum:", a + b)
+
+# Гіпотенуза: За двома катетами прямокутного трикутника знайти довжину його гіпотенузи.
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("hypotenuse:", (a + b) / 2)
+
+# Двічі: Написати програму, яка зчитує ціле число та виводить його двічі.
+
+# a = int(input("Enter first number: "))
+# print("Here is your number: ", a)
+# print("And again: ", a)
+
+# Привіт, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить привітання.
+
+# print("I require your name!",)
+# a = input()
+# print("Greetings, servant " + a + "!")
+
+# Прощавай, <ім'я>: Написати програму, яка запитує ім'я користувача і виводить прощання.
+
+# print("Identify youself",)
+# a = input()
+# print("Farewell, human " + a + "!")
+
+# Вік через рік: Напишіть програму, яка запитує ім'я користувача та його вік. Потім виведіть повідомлення,
+# яке повідомляє, скільки йому буде років через рік.
+
+# print("Name?",)
+# a = input()
+# print("Age?",)
+# b = input()
+# print("In one year," + a + "you will be" + (b + 1) +".")
+
+# Площа прямокутника: Напишіть програму, яка запитує довжину та ширину прямокутника,
+# а потім обчислює та виводить його площу.
+# a = int(input("Enter length of rectangle in cm: "))
+# b = int(input("Enter width of rectangle in cm: "))
+# print ("Area of the rectangle is " + str(a * b) + "cm²")
+
+#Конвертер валют: Запропонуйте користувачеві ввести суму в одній валюті (наприклад, доларах),
+# а потім виведіть цю суму в іншій валюті (наприклад, євро), використовуючи заздалегідь
+#визначений коефіцієнт обміну.
+
+# k = 0.86
+# print("Enter your sum in $",)
+# a = int(input())
+# b = a / k
+# print("You will get " + str(b) + "€")
+
+#Вартість покупки: Запитайте користувача назву товару, його ціну та кількість.
+# Обчисліть загальну вартість покупки та виведіть її на екран.
+
+# print("Enter product title",)
+# a = input()
+# print("Enter " + a + " price in €",)
+# b = int(input())
+# print("Enter amount",)
+# c = int(input())
+# print ("Price in total is " + str(b * c) + "€")
+
+#Температура: Напишіть програму, яка перетворює температуру з градусів Цельсія на Фаренгейт.
+# Запитайте користувача температуру в градусах Цельсія.
+
+# print("Enter temperature in °C",)
+# a = int(input())
+# print ("Temperature is " + str((a * 9 / 5) + 32) + "°F")
+
+
+
+
+
+
