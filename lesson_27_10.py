14ae88e753087b1e6b8d76e2e07a7def412a2dfb
Taras <t.kornev01@gmail.com>
2025-10-27 11:18:41 +0100

    #	new file:   tasks_for_while.py

---
+++ lesson_27_10.py
@@ -0,0 +1,56 @@
+# #ax^2 + bx + c = 0
+# a = int(input("a: "))
+# b = int(input("b: "))
+# c = int(input("c: "))
+
+# print(f"дано кв уравнение {a} x2 {b} x + {c} = 0")
+
+# d = b ** 2 - 4 * a * c
+
+# if d < 0:
+#     print("коренiв нема")
+# elif d == 0:
+#     x = (-b) / (2 * a)
+#     print("корень один: х =", x)
+# else:
+#     x1 = (-b -d ** 0.5) / (2 * a)
+#     x1 = (-b -d ** 0.5) / (2 * a)
+
+ #------------------------------------------------------
+
+
+
+# n = int(input("Enter number: "))
+# for i in range(n):
+#     print("Hello Python!")
+#--------------------------------------------------
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+
+# if a > b:
+#     a, b = b, a
+# 5
+# for i in range(a, b + 1):
+#     print(i)
+
+#-------------------------------------------
+
+# n = int(input("Enter sec: "))
+# for i in range(n, 0, -1):
+#     print(i)
+# print("Start!")
+#--------------------------------------------
+# n = int(input("Enter sec: "))
+# while n > 0:
+#     print(n)
+#     n -= 1
+# print("Start!")
+#-------------------------------------------------
+# n = int(input("Enter number of steps: "))
+# for i in range(1, n + 1):
+#     print("#" * i)
+
+# n = int(input("Enter number of steps: "))
+# for i in range(1, n + 1):
+#     print("#" * i)
